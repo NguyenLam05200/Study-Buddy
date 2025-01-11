409761921cae1bc0125ff745966ccd6673c195d6
NguyenLam0502 <lamnv@nami.trade>
2025-01-11 17:19:29 +0700

    fix: change icon logo

--- app/src/main/java/com/example/studybuddy/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.studybuddy
 import android.os.Bundle
 import android.view.Menu
 import android.view.ViewGroup
+import android.widget.Switch
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -12,6 +13,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.studybuddy.databinding.ActivityMainBinding
@@ -38,6 +40,26 @@ class MainActivity : AppCompatActivity() {
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         val headerView = navView.getHeaderView(0) // Lấy header layout
+
+        // Lấy Switch từ header layout
+        val switchButton = headerView.findViewById<Switch>(R.id.switchButton)
+
+        // Sử dụng SharedPreferences
+        val sharedPreferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+
+        // Khởi tạo trạng thái Switch dựa trên SharedPreferences
+        val isDarkMode = sharedPreferences.getBoolean(SWITCH_BUTTON_KEY, false)
+        switchButton.isChecked = isDarkMode
+        updateUIMode(isDarkMode)
+
+        // Lắng nghe sự kiện thay đổi trạng thái của Switch
+        switchButton.setOnCheckedChangeListener { _, isChecked ->
+            editor.putBoolean(SWITCH_BUTTON_KEY, isChecked).apply()
+            updateUIMode(isChecked)
+        }
+
+        // Đặt layout padding theo SystemBars
         ViewCompat.setOnApplyWindowInsetsListener(headerView) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             val layoutParams = v.layoutParams as ViewGroup.MarginLayoutParams
@@ -68,4 +90,22 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+    private fun updateUIMode(isDarkMode: Boolean) {
+        val currentMode = resources.configuration.uiMode and android.content.res.Configuration.UI_MODE_NIGHT_MASK
+        val newMode = if (isDarkMode) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+
+        if ((currentMode == android.content.res.Configuration.UI_MODE_NIGHT_YES && !isDarkMode) ||
+            (currentMode == android.content.res.Configuration.UI_MODE_NIGHT_NO && isDarkMode)) {
+            AppCompatDelegate.setDefaultNightMode(newMode)
+        }
+    }
+
+
+    companion object {
+        const val SWITCH_BUTTON_KEY = "switch"
+        const val PREF_KEY = "pref"
+    }
+
 }
+
