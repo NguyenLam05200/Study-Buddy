933e9e5a468fc262466616556a167d3fb0665f57
NguyenLam0502 <lamnv@nami.trade>
2025-01-11 20:44:11 +0700

    feat: add another nav Course

--- app/src/main/java/com/example/studybuddy/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy/MainActivity.kt
@@ -73,7 +73,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_course
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/example/studybuddy/ui/course/CourseViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.studybuddy.ui.course
+
+import androidx.lifecycle.ViewModel
+
+class CourseViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
